4aa821d1141eb4ee46664934fb18a87b7ae172d1
Dhruv Sharma <dhruvsharma@localhost.localdomain>
2018-10-27 10:54:31 -0400

    update DemoApplictation.main

--- webapp/demo/src/main/java/demo/DemoApplication.java
+++ webapp/demo/src/main/java/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
-
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
 		return application.sources(DemoApplication.class);
 	}
