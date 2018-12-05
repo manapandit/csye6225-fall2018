9b32c0e67e1fe8d176480600f818850de230c81d
paduthol-jishnu <43180733+paduthol-jishnu@users.noreply.github.com>
2018-12-05 05:40:10 -0500

    Update AmazonClient.java

--- webapp/demo/src/main/java/demo/Service/AmazonClient.java
+++ webapp/demo/src/main/java/demo/Service/AmazonClient.java
@@ -88,8 +88,8 @@ public class AmazonClient {
     }
 
     private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(bucketName, fileName, file));
-        //withCannedAcl(CannedAccessControlList.PublicRead));
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
     }
 
     public String deleteFileFromS3Bucket(String fileUrl) {
