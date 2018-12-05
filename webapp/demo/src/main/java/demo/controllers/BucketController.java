7d7552e79a1165641500c85c2b2c45cbf2945939
Dhruv Sharma <dhruvsharma@localhost.localdomain>
2018-12-04 21:32:35 -0500

    controller-changes

--- webapp/demo/src/main/java/demo/controllers/BucketController.java
+++ webapp/demo/src/main/java/demo/controllers/BucketController.java
@@ -9,6 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,7 @@ import demo.Service.AmazonClient;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Properties;
 import java.util.UUID;
-
+@Profile("Dev")
 @RestController
 @PropertySource("classpath:application.properties")
 @RequestMapping("/transaction/{id}")
